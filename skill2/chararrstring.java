c715169b3e294108497da7378d0619e8d1eff8ed
KrishitaMyneni <krishitamyneni5@gmail.com>
2025-08-09 19:26:41 +0530

    Add files via upload

---
+++ skill2/chararrstring.java
@@ -0,0 +1,16 @@
+package skill2;
+import java.util.*;
+public class chararrstring {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		char[] charr = {'s','t','r','i','n','g'};
+		String s ;
+		s = sc.nextLine();
+		String newstring = new String(charr);
+		System.out.println("Character array: "+ newstring +", String object: "+s);
+
+	}
+
+}
---
+++ skill2/stringbuilder.java
@@ -0,0 +1,15 @@
+package skill2;
+import java.util.*;
+public class stringbuilder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = 20;
+		Integer i = Integer.valueOf(a);
+		Integer j = a;
+		System.out.println(a+" "+i+" "+j);
+
+	}
+
+}
